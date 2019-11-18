6c76cb58944b3224847864d1b5ed3f7789ca1660
ANUPOJM <madhav@example.com>
2019-11-18 15:45:19 +0530

    Added DB queries

---
+++ customer-service/src/main/java/com/customer/app/commons/DBQueries.java
@@ -0,0 +1,9 @@
+package com.customer.app.commons;
+
+public interface DBQueries {
+	String SELECT_ALL_CUSTMR = "";
+	String SELECT_CUSTMR_ID = "";
+	String INSERT_CUSTMR = "";
+	String UPDTE_CUSTMR_BY_ID = "";
+	String DELTE_CUSTMR_BY_ID = "";
+}

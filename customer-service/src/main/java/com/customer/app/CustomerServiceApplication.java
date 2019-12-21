82f4dc65910435c3d4cb6db3d3c344a3db3cb632
dell <anupoju.madhav@gmail.com>
2019-12-21 13:42:08 +0530

    Display the loaded beans

--- customer-service/src/main/java/com/customer/app/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/customer/app/CustomerServiceApplication.java
@@ -1,9 +1,13 @@
 package com.customer.app;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -12,7 +16,15 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-	
+	@Bean
+    public CommandLineRunner run(ApplicationContext appContext) {
+        return args -> {
+
+            String[] beans = appContext.getBeanDefinitionNames();
+            Arrays.stream(beans).sorted().forEach(System.out::println);
+
+        };
+    }
 	
 
 }
